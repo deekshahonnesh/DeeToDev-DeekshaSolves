f8bc530d0e904d60c1f178ab13d6b172d4e9f7d1
Deeksha C H <deekshahonnesh2009@gmail.com>
2026-09-21 21:25:44 +0530

    Time: 360 ms (60.53%) | Memory: 34.2 MB (69.74%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -8,15 +8,13 @@ class Solution:
 
     
         for num in nums:
-            current_remainder = num % k
-        # next_dp calculates the updated counts for subarrays ending at this element
+            current_remainder=num%k
             next_dp = [0] * k
         
-        # Choice 1: Start a brand-new single-element subarray at this number
+        
             next_dp[current_remainder] += 1
         
-        # Choice 2: Extend all existing past subarrays by multiplying them 
-        # by our current element's remainder
+        
             for prev_remainder in range(k):
                 if dp[prev_remainder] > 0:
                     new_remainder = (prev_remainder * current_remainder) % k
@@ -24,7 +22,7 @@ class Solution:
             for remainder in range(k):
                 result[remainder] += next_dp[remainder]
             
-        # Move the sliding window forward to process the next element
+        
             dp = next_dp
         
         return result
